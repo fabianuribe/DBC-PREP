de885e07335073f0f1b4049bd6b2349b7f87905a
Fabian Uribe <fabian.uribe@gmail.com>
2013-07-08 12:45:52 -0500

    Add all Ruby Koans excercises

---
+++ koans/triangle.rb
@@ -0,0 +1,30 @@
+# Triangle Project Code.
+
+# Triangle analyzes the lengths of the sides of a triangle
+# (represented by a, b and c) and returns the type of triangle.
+#
+# It returns:
+#   :equilateral  if all sides are equal
+#   :isosceles    if exactly 2 sides are equal
+#   :scalene      if no sides are equal
+#
+# The tests for this method can be found in
+#   about_triangle_project.rb
+# and
+#   about_triangle_project_2.rb
+#
+def triangle(a, b, c)
+
+	raise TriangleError, "All sides must be positive and greater than zero" if [a,b,c].min <= 0
+	raise TriangleError, "This is not a valid triangle" if a+b <= c || a+c <= b || b+c <= a
+
+  if a == b || b == c || a == c
+  	return (a + b + c) / 3.0 == a  ? :equilateral : :isosceles
+  else
+  	return :scalene
+  end
+end
+
+# Error class used in part 2.  No need to change this code.
+class TriangleError < StandardError
+end
